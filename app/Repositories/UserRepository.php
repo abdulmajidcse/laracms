ca62a59cdc68f60c60678cfce435f9ba53909c20
abdulmajidcse <abdulmajidcse@gmail.com>
2025-10-13 07:46:39 +0600

    Update user repository

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,12 +3,18 @@
 namespace App\Repositories;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 
 class UserRepository
 {
+    private function query(): Builder
+    {
+        return User::query();
+    }
+
     public function findByEmail(string $email): ?User
     {
-        return User::query()
+        return $this->query()
             ->where('email', $email)
             ->first();
     }
